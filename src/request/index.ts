4f8ae814685bb14f734ecb165a3ca27c50a23323
Alessio Frittoli <info@alessiofrittoli.it>
2025-01-02 21:05:06 +0100

    feat: add `getRequestIp` utility function

---
+++ src/request/index.ts
@@ -0,0 +1,18 @@
+import type { NextRequest } from 'next/server'
+
+
+/**
+ * Get request IP address.
+ *
+ * @param	request The NextRequest Instance.
+ * @returns	The Request IP address.
+ */
+export const getRequestIp = async ( request?: NextRequest ) => {
+
+	const headers		= request?.headers || await ( await import( 'next/headers' ) ).headers()
+	const forwarded		= headers.get( 'X-Forwarded-For' )?.replace( /\s/g, '' ).split( ',' ).at( -1 )
+	const realIp		= headers.get( 'X-Real-Ip' )?.replace( /\s/g, '' ).split( ',' ).at( -1 )
+
+	return forwarded || realIp || null
+
+}
