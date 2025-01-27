707883afe9b7321758656df8db99dc9cde73a282
Alessio Frittoli <info@alessiofrittoli.it>
2025-01-27 10:25:55 +0100

    add `middlewares` to build entries and export path in `package.json`

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig( {
 	entry: [
-		'src/index.ts', 'src/error/index.ts',
+		'src/index.ts', 'src/error/index.ts', 'src/middlewares/index.ts',
 		'src/request/index.ts', 'src/response/index.ts',
 		'src/route-wrappers/index.ts', 'src/types/index.ts',
 	],
