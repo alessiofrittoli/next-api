c4f43cd35b07574a528fbe2d71e57d6defeb3109
Alessio Frittoli <info@alessiofrittoli.it>
2025-12-14 03:03:31 +0100

    chore: deprecated and renamed `middleware` related functions
    will be removed in the next major release since they're now renamed with `proxy` in order to match latest Next.js versions

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,28 +1,62 @@
-import type { NextMiddleware } from 'next/server'
+import type { NextProxy } from 'next/server'
 import type { ChainFactory, ChainLink, LastChainLink } from '@alessiofrittoli/chain-functions/types'
 import { NextResponse } from '@/response'
 
 
 /**
  * Next.js Middleware chained using [`Chain.functions()`](https://npmjs.com/package/@alessiofrittoli/chain-functions).
+ * 
+ * @deprecated use {@link Proxy} instead. Middleware has been renamed to Proxy.
  */
-export type Middleware = ChainLink<NextMiddleware>
+export type Middleware = ChainLink<NextProxy>
+
+/**
+ * Next.js Middleware chained using [`Chain.functions()`](https://npmjs.com/package/@alessiofrittoli/chain-functions).
+ */
+export type Proxy = Middleware
 
 
 /**
  * Last Next.js Middleware chained using [`Chain.functions()`](https://npmjs.com/package/@alessiofrittoli/chain-functions).
+ * 
+ * @deprecated use {@link LastProxy} instead. Middleware has been renamed to Proxy.
  */
 export type LastMiddleware = () => NextResponse<unknown>
 
 
 /**
- * Next.js Middlewares chain passed to [`Chain.functions()`](https://npmjs.com/package/@alessiofrittoli/chain-functions).
+ * Last Next.js Middleware chained using [`Chain.functions()`](https://npmjs.com/package/@alessiofrittoli/chain-functions).
  */
-export type MiddlewareFactory = ChainFactory<NextMiddleware, LastMiddleware>
+export type LastProxy = LastMiddleware
+
+
+/**
+ * Next.js Middlewares chain passed to [`Chain.functions()`](https://npmjs.com/package/@alessiofrittoli/chain-functions).
+ * 
+ * @deprecated use {@link ProxyFactory} instead. Middleware has been renamed to Proxy.
+ */
+export type MiddlewareFactory = ChainFactory<NextProxy, LastMiddleware>
+
+
+/**
+ * Next.js Middlewares chain passed to [`Chain.functions()`](https://npmjs.com/package/@alessiofrittoli/chain-functions).
+ * 
+ * @deprecated use {@link ProxyFactory} instead. Middleware has been renamed to Proxy.
+ */
+export type ProxyFactory = MiddlewareFactory
+
+
+/**
+ * The last Next.js Middleware chained using [`Chain.functions()`](https://npmjs.com/package/@alessiofrittoli/chain-functions).
+ * This simply calls `NextResponse.next()`.
+ * 
+ * @deprecated use {@link lastProxy} instead. Middleware has been renamed to Proxy.
+ */
+export const lastMiddleware: LastChainLink<LastMiddleware> = () => () => NextResponse.next()
 
 
 /**
  * The last Next.js Middleware chained using [`Chain.functions()`](https://npmjs.com/package/@alessiofrittoli/chain-functions).
  * This simply calls `NextResponse.next()`.
  */
-export const lastMiddleware: LastChainLink<LastMiddleware> = () => () => NextResponse.next()
+export const lastProxy = lastMiddleware
